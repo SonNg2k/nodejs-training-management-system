e09ea933aee57e9570494da0ea2f2bcd319f1f42
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-09-03 16:55:55 +0700

    Add programID to Session schema for easy deletion

--- models/Session.js
+++ models/Session.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
+const { schema } = require('./Program')
 
 const SessionSchema = new mongoose.Schema({
+    programID = {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Program',
+        required: true
+    },
     name: {
         type: String,
         required: true,
