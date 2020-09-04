076f5d527c0afbab81992cebb17638848e6a313f
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-09-04 13:30:43 +0700

    Configure to fetch a list of items on first render

--- client/src/components/MultiSelector.js
+++ client/src/components/MultiSelector.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated';
-import seedData from '../seedData'
+
+import { fetchListOf } from '../helpers'
 
 const animatedComponents = makeAnimated();
 
 // let the user select multiple training sessions/programs
-export default function MultiSelector({updateListOfParent, chosenItems, itemsBelongTo}) {
-    const itemList = seedData[itemsBelongTo]
+export default function MultiSelector({ updateListOfParent, chosenItems, itemsBelongTo }) {
+    const [itemList, setItemList] = useState([])
 
+    useEffect(() => {
+        fetchListOf(itemsBelongTo)
+            .then(({ data: list }) => setItemList(list))
+    }, [])
     return (
         <Select
             closeMenuOnSelect={false}
@@ -21,7 +26,7 @@ export default function MultiSelector({updateListOfParent, chosenItems, itemsBel
             getOptionValue={(option) => option._id} // by default, react-select search for 'value' key --> make the '_id' as its alias
             getOptionLabel={(option) => option.name} // by default, react-select search for 'label' key --> make the 'name as its alias
             onChange={(currentChosenItems) => updateListOfParent(currentChosenItems)}
-            // items can be either training sessions or training programs
+        // items can be either training sessions or training programs
         />
         // Remember that the keys are in the following format by default --> itemList = [{value: ..., label: ...}, {}, ...]
     )
