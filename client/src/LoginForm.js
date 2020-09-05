8b6ab1a501e564a1886ee1775753669f037d75b1
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-09-05 15:25:07 +0700

    LoginForm can now send credentials and auth user

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 
 class LoginForm extends React.Component {
-    state = { email: "", password: "" }
+    state = { email: "", password: "", loginFailed: false }
 
     onInputChange = (e) => {
         const { name, value } = e.target
@@ -15,17 +15,24 @@ class LoginForm extends React.Component {
 
     onFormSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state)
+        axios.post('/', this.state)
+            .then(({ data: { role, token } }) => {
+                localStorage.setItem('token', token)
+                this.props.loginWithRole(role)
+            })
+            .catch(() => this.setState({ loginFailed: true }))
     }
 
     render() {
-        const { email, password } = this.state
+        const { email, password, loginFailed } = this.state
         return (
             <Container>
-                <Form onSubmit={this.onFormSubmit} className="mt-5 mx-auto" style={{width: "300px"}}>
+                <Form onSubmit={this.onFormSubmit} className="mt-5 mx-auto" style={{ width: "300px" }}>
+                    {loginFailed && <h5 className='text-danger'> 🚨Wrong username or password, please check again!</h5>}
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control required name="email" type="email" placeholder="Enter email" onChange={this.onInputChange} />
+                        <Form.Control required name="email" value={email} type="email" onChange={this.onInputChange}
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                     </Form.Text>
@@ -33,11 +40,9 @@ class LoginForm extends React.Component {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control required name="password" type="password" placeholder="Password" onChange={this.onInputChange} />
+                        <Form.Control required name="password" value={password} type="password" onChange={this.onInputChange} />
                     </Form.Group>
-                    <Button variant="warning" type="submit" style={{marginLeft: "40%"}}>
-                        Login
-                </Button>
+                    <Button variant="warning" type="submit" style={{ marginLeft: "40%" }}>Login</Button>
                 </Form>
             </Container>
         )
