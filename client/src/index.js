8bdeb18058b1dcbc00534870f6c42591e84d9c43
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-09-05 16:24:59 +0700

    Big changes to /src/index.js
    + Attach JWT token to every single request to server
    
    + Create LoginControl which renders different layout based on role

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
+import LoginForm from './LoginForm'
+import ManagerLayout from './components/ManagerLayout'
 import TraineerLayout from './components/TraineerLayout'
-import AdminLayout from './components/AdminLayout'
-import AssistantLayout from './components/AssistantLayout'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
 
 class App extends React.Component {
     render() {
+        return <LoginControl />
+    }
+}
+
+class LoginControl extends React.Component {
+    state = { isLoggedIn: false, role: '' }
+
+    loginWithRole = (role) => this.setState({ isLoggedIn: true, role: role })
+
+    logout = () => {
+        localStorage.removeItem('token')
+        this.setState({ isLoggedIn: false, role: '' })
+    }
+
+    render() {
+        const { isLoggedIn, role } = this.state
+        const layoutFor = {
+            admin: ManagerLayout,
+            assistant: ManagerLayout,
+            trainer: TraineerLayout,
+            trainee: TraineerLayout
+        }
+        const Layout = layoutFor[role]
 
         return (
-            // <Router>
-            //     <TraineerLayout pageFor='trainer'/>
-            // </Router>
             <>
-                <AssistantLayout />
-                {/* <AlertBoard msg='Operation failed ❌' bkgColor='danger' /> */}
+                {!isLoggedIn && <LoginForm loginWithRole={this.loginWithRole} />}
+                {isLoggedIn && <Layout role={role} logout={this.logout} />}
             </>
         )
     }
