44e0f7fe5ca04090ac2571ed8aa9e0e86303f1ee
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-09-05 19:47:45 +0700

    Allow trainer/trainee to update the profile in DB

--- client/src/components/forms/TraineerProfile.js
+++ client/src/components/forms/TraineerProfile.js
@@ -1,19 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 import Form from 'react-bootstrap/Form'
 import { EditBtn } from '../myReusable'
 
 import { UserBasicFields, TrainerUniqueFields, TraineeUniqueFields } from './reusableFields'
-import { handleSubmit } from '../../helpers'
 
 class TraineerProfile extends React.Component {
-    state = this.props.profile // there is always a detailed profile passed from parent
+    state = this.props.profile // there is always a profile passed from parent
 
     onInputChange = (e) => {
         if (!e) return
         if (e instanceof Date) return this.setState({ dob: e }) // the DatePicker passes the Date obj to 'e' parameter
         const { name, value } = e.target
         this.setState({ [name]: value })
+    }
+
+    onFormSubmit = (e) => {
+        const form = e.currentTarget;
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({ validated: true })
+
+        const { showAlert, closeModal, role } = this.props
+        if (form.checkValidity())
+            axios.put(`/${role}s`, this.state)
+                .then(() => {
+                    closeModal()
+                    showAlert()
+                })
     }
 
     render() {
@@ -25,7 +40,7 @@ class TraineerProfile extends React.Component {
         const UniqueFields = uniqueFields[role]
 
         return (
-            <Form noValidate validated={this.state.validated} onSubmit={(e) => handleSubmit(e, this)}>
+            <Form noValidate validated={this.state.validated} onSubmit={this.onFormSubmit}>
                 <UserBasicFields parentState={this.state} onInputChange={this.onInputChange} btnToShow='editBtn' />
                 <UniqueFields parentState={this.state} onInputChange={this.onInputChange} />
                 <EditBtn text={`Update your ${role}  profile`} className="center" />
